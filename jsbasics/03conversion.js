960fda1afefcd09b93486d8cdbadcb1169de5571
Aman Kumar <115198251+AmanKumar157@users.noreply.github.com>
2025-02-09 12:26:06 +0000

    conversion of  data-types

---
+++ jsbasics/03conversion.js
@@ -0,0 +1,23 @@
+const a="123"
+
+console.log(typeof a)
+
+//conversion
+let stringinnumber=Number(a)
+
+console.log(typeof stringinnumber)
+
+
+const b="123abc"
+console.log(typeof b)
+ let bnumber=Number(b)
+ console.log(typeof bnumber)   //number
+ console.log(bnumber)   //NaN =>not a number=> type- Number
+ 
+
+ //null=> 0
+ //undefined=>NaN
+ //true=>1
+ //false=>0
+ //"hitesh"=>Na
+
