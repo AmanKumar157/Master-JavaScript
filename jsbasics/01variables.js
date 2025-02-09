b09866e8769b5cb4a4cd3dc054587df7407fe9cd
Aman Kumar <115198251+AmanKumar157@users.noreply.github.com>
2025-02-09 10:27:52 +0000

    container config done

---
+++ jsbasics/01variables.js
@@ -0,0 +1,23 @@
+const id=123
+let accountEmail="aman123@gmail.com"
+var accountPas="121234"
+city = "Noida" 
+// possible but not recommened
+
+
+let state;
+//undefined
+
+
+
+//  id=14 not allowed in js 
+
+accountEmail="aman444@gmail.com"
+accountPas="1456"
+city="bengluru"
+
+console.table([id,accountEmail,accountPas,city,state])
+
+
+// prefer not to use var
+// bcz ofissue in block scope and function scope 
