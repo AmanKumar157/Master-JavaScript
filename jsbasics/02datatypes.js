86384531ffa4c46928b8b84d7cd4e9682eb4227a
Aman Kumar <115198251+AmanKumar157@users.noreply.github.com>
2025-02-09 11:45:40 +0000

    datatypes

---
+++ jsbasics/02datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+//treat all js code newer version
+
+
+ const num=123;
+ let s="aaman"
+ let isloggedIn=true;
+ let temp=null;
+
+//primitive data-type
+
+ //number =>+- 2 ki poweer53 -1
+ //string => a=""
+ //bigInt
+ //boolean
+ //NULL=> standalone value
+ //undefined => let a;
+ //Symbole =>uniquenesss
+
+ //non -primitive data-types
+ //object
+ //fuction
+
+ console.log(typeof(s))    //to know the data type of variable
+ console.log(typeof null)  //object
+ console.log(typeof undefined)  //undefined
+
+ 
