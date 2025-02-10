9b1504dfb679aa46d0cfff469dcc8c68a03b6133
Aman Kumar <115198251+AmanKumar157@users.noreply.github.com>
2025-02-10 10:43:01 +0000

    data type summry

---
+++ jsbasics/05datatypes_summ.js
@@ -0,0 +1,24 @@
+//data types=> primitive and non primitive
+
+// kis tarah se store hote hain aur kis tarah se acess kar sakte hain usi lk basis me divide kiya gaya hain
+
+//primitive => call by value=>original data ki memory ka refrence nhi diya jata uskicopyn di jati hain 
+//7=> string, number ,bollean ,null,undefined , bigint,symbol
+
+
+//refernce type (non primitive)
+//array
+//objects
+//functions
+
+const heros=["Aman", "saktiman", "spiderman",""]
+//object
+let myobj= {
+    name:"aman",
+    age:18,
+}
+
+//function
+const myfun=function(){
+    console.log("hello world")
+}
