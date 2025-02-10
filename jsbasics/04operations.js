ebafb7c29f31087b776f7d226597ea827086f373
Aman Kumar <115198251+AmanKumar157@users.noreply.github.com>
2025-02-10 10:17:33 +0000

    operations

---
+++ jsbasics/04operations.js
@@ -0,0 +1,32 @@
+let val=3
+let negvalue=-val
+console.log(negvalue)
+
+//arithmatic operators
+console.log(2+2)  //addition
+console.log(2-2)   //sub
+console.log(2*2)   //multiplication
+console.log(2**4)  //power
+console.log(2/4)   //divide
+console.log(5%2)   // modulus => gives remainder
+
+
+
+let str1="aman"
+let str2=" kumar"
+console.log(str1+str2)
+
+console.log("2"+2)
+console.log(2+"2")
+console.log(2+2+"3")
+console.log("3"+2+2)
+
+
+
+//comparision operators
+//<,>,<=,>=,!=,==,===
+console.log("2">1)
+console.log("02">1)
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
